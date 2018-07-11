cb129dff2f1c89f0682cf6cb920ff8a4141117f5
Zakerberg <blmichael1129@icloud.com>
2018-07-11 15:10:14 +0800

    Commit

--- shixiUS/ShixiUS/Classes/Main/SX_LoadingView.swift
+++ shixiUS/ShixiUS/Classes/Main/SX_LoadingView.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class SX_LoadingView: UIView {
 
-    typealias LoadingFailedBlock = (_ button: UIButton) -> Void
-    
+    typealias LoadingFailedClosure = ((_ button: UIButton) -> ())
+
+    //    /// 请求成功Closure
+    //    typealias HttpRequestSuccessClosure = ((_ responseData: Any) -> ())?
+    //    /// 请求失败的Closure
+    //    typealias HttpRequestFailClosure    = ((_ Error: NSError) -> ())?
+    //    /// 请求响应Closure
+    //    typealias HttpRequestClosure        = ((_ dataObj: Any, _ error: NSError) -> ())?
+    //    /// 监听进度响应Closure
+    //    typealias HttpProgressClosure       = ((_ progress: Progress) -> ())?
+
     var indicatorView: UIImageView?
     var failIndicatorLabel: UILabel?
     var failView: UIImageView?
     var failLabel: UILabel?
     var reloadButton: UIButton?
-    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -50,18 +58,63 @@ extension SX_LoadingView  {
         self.indicatorView?.animationRepeatCount = 0
         indicatorView?.startAnimating()
         
-        self.failView = UIImageView()
+        self.failView?.frame = CGRect(x: SCREEN_WIDTH/2-100/4, y: SCREEN_HEIGHT/2-71/2-50-20, width: 50, height: 71/2)
+        self.failView = UIImageView(image: UIImage.init(named: "icon_wifi")).addhere(toSuperView: self).config({ (failView) in
+            failView.alpha = 0
+        })
         
- 
+        self.failLabel = UILabel().addhere(toSuperView: self).layout(snapKitMaker: { (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.failView!.snp.bottom).offset(20)
+        }).config({ (failLabel) in
+            failLabel.text = "界面加载失败"
+            failLabel.numberOfLines = 2
+            failLabel.font = UIFont.systemFont(ofSize: 13)
+            failLabel.alpha = 0
+            failLabel.textAlignment = .center
+            failLabel.textColor = UIColor.colorWithHexString(hex: "999999", alpha: 1)
+        })
         
         
+        self.reloadButton = UIButton(type: .custom).addhere(toSuperView: self).layout(snapKitMaker: { (make) in
+            make.top.equalTo(self.failLabel!.snp.bottom).offset(23)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(CGSize(width: 90, height: 28))
+        }).config({ (reloadButton) in
+            reloadButton.setTitle("重新加载", for: .normal)
+            reloadButton.titleLabel?.font = UIFont.systemFont(ofSize: 13)
+            reloadButton.layer.borderWidth = 0.5
+            reloadButton.layer.cornerRadius = 3
+            reloadButton.layer.borderColor = UIColor.colorWithHexString(hex: "999999", alpha: 1).cgColor
+
+            reloadButton.alpha = 0
+            reloadButton.setTitleColor(UIColor.colorWithHexString(hex: "666666", alpha: 1), for: .normal)
+            reloadButton.addTarget(self, action: #selector(reloadButtonClick), for: .touchUpInside)
+        })
+    }
+    
+    /// hideLoadingView
+    func hideLoadingView() {
+        UIView.animate(withDuration: 0.1, animations: {
+        self.alpha = 0
+        }) { (finished) in
+            self.removeFromSuperview()
+        }
+    }
+    
+    /// loadingFailed
+    func loadingFail() {
+        self.indicatorView?.alpha = 0
+        self.failLabel?.alpha = 1
+        self.failView?.alpha = 1
+        self.reloadButton?.alpha = 1
+    }
+    
+    ///
+    @objc func reloadButtonClick(btn: UIButton) {
+      
+      
         
         
     }
 }
-
-
-
-
-
-
