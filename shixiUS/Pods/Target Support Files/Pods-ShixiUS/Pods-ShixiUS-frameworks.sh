ca3ab8f003c9142a5333e7bd641546fa52fd1060
Zakerberg <blmichael1129@icloud.com>
2018-08-01 17:34:34 +0800

    Commit

--- shixiUS/Pods/Target Support Files/Pods-ShixiUS/Pods-ShixiUS-frameworks.sh
+++ shixiUS/Pods/Target Support Files/Pods-ShixiUS/Pods-ShixiUS-frameworks.sh
@@ -136,6 +136,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PagingMenuController/PagingMenuController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveSwift/ReactiveSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Result/Result.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
@@ -145,6 +146,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PagingMenuController/PagingMenuController.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ReactiveSwift/ReactiveSwift.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Result/Result.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/RxCocoa/RxCocoa.framework"
--- shixiUS/ShixiUS/Classes/Home/View/SX_TrainingProject/SX_ProjectDetailInstructionsView.swift
+++ shixiUS/ShixiUS/Classes/Home/View/SX_TrainingProject/SX_ProjectDetailInstructionsView.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class SX_ProjectDetailInstructionsView: UIView {
-
+    
+    typealias titleClickClosure = ((NSInteger)->Void)
+    var titleClosure: titleClickClosure?
+    
     var titles: Array<Any>?
     var bottomView: UIView?
+    var indicateLine:UIView?
+    var instructionBtn: UIButton?
+    var titleBtnArray :NSMutableArray?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        ConfigCell()
         
     }
     
@@ -27,12 +34,65 @@ class SX_ProjectDetailInstructionsView: UIView {
 // MARK: - ConfigCell()
 // ===============================================================================================================================
 extension SX_ProjectDetailInstructionsView {
-
+    
     func ConfigCell() {
         
+        self.frame = CGRect(x: 0, y: 0, width: SCREEN_WIDTH, height: 40.FloatValue.IPAD_XValue)
+        self.backgroundColor = UIColor.green
+        let titleArr = ["项目亮点","日程安排","费用说明","预定须知"]
+        let btnWidth = SCREEN_WIDTH/4
         
+        for i in 0...titleArr.count {
+            self.instructionBtn = UIButton(type: .custom).addhere(toSuperView: self).layout(snapKitMaker: { (make) in
+                make.top.equalToSuperview()
+                make.left.equalToSuperview().offset(i*Int(btnWidth))
+                make.width.equalTo(btnWidth)
+                make.height.equalToSuperview()
+            }).config { (btn) in
+                btn.setTitle(titleArr[i], for: .normal)
+                if i == 0 {
+                    btn.setTitleColor(UIColor.black, for: .normal)
+                }else{
+                    btn.setTitleColor(UIColor.SX_MainColor(), for: .normal)
+                }
+                btn.setTitleColor(UIColor.SX_MainColor(), for: .selected)
+                btn.tag = i
+                self.titleBtnArray?.add(btn)
+                
+                btn.rx.tap.subscribe(onNext: { (_) in
+                    SXLog("点击了按钮\(btn.title)")
+                    let tag = btn.tag
+                    
+                    self.setItemSelected(colunm: tag)
+                    if (self.titleClosure != nil) {
+                        self.titleClosure!(tag)
+                    }
+                    
+                }, onError: { (error) in
+                    SXLog(error)
+                }, onCompleted: nil, onDisposed: nil)
+            }
+        }
         
-        
+        self.indicateLine = UIView().addhere(toSuperView: self).layout(snapKitMaker: { (make) in
+            make.width.equalTo(self.instructionBtn!)
+            make.width.equalTo(1.FloatValue.IPAD_XValue)
+            make.top.equalTo(self.instructionBtn!.snp.bottom).offset(1)
+        }).config({ (indicateLine) in
+            indicateLine.backgroundColor = UIColor.SX_MainColor()
+        })
+    }
+    
+    func setItemSelected(colunm: NSInteger) {
+        for index in 0...self.titleBtnArray!.count {
+            let btn = titleBtnArray![index] as! UIButton
+            if index == colunm {
+                btn.setTitleColor(UIColor.SX_MainColor(), for: .normal)
+            }else{
+                btn.setTitleColor(UIColor.colorWithHexString(hex: "ff7900", alpha: 1), for: .normal)
+            }
+        }
+        let btnWidth = SCREEN_WIDTH/CGFloat(self.titleBtnArray!.count)
+        self.indicateLine?.frame = CGRect(x: btnWidth*CGFloat(colunm), y: 49, width: btnWidth, height: 1)
     }
 }
-
