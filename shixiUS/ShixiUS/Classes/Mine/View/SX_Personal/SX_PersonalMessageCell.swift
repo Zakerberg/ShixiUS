bebe858de1fa39e1e3f846df63b5c060ffa14cea
Zakerberg <blmichael1129@icloud.com>
2018-08-09 15:09:44 +0800

    Commit

--- shixiUS/ShixiUS/Classes/Mine/View/SX_Personal/SX_PersonalMessageCell.swift
+++ shixiUS/ShixiUS/Classes/Mine/View/SX_Personal/SX_PersonalMessageCell.swift
@@ -10,8 +10,8 @@ import UIKit
 
 class SX_PersonalMessageCell: UITableViewCell {
 
+    var titleLabel   : UILabel?
     var tF           : UITextField?
-    var countryLabel : UILabel?
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -41,16 +41,24 @@ extension SX_PersonalMessageCell {
     
     func ConfigCell() {
         
-        self.tF = UITextField().addhere(toSuperView: self.contentView).layout(snapKitMaker: { (make) in
-        
-        }).config({ (tF) in
-
+        self.titleLabel = UILabel().addhere(toSuperView: self.contentView).layout(snapKitMaker: { (make) in
+            make.left.top.equalToSuperview().offset(Margin)
+            make.width.equalTo(80.FloatValue.IPAD_XValue)
+            make.height.equalTo(20.FloatValue.IPAD_XValue)
+        }).config({ (Title) in
+            Title.sizeToFit()
+            Title.font = UIFont.systemFont(ofSize: 15)
+            Title.textColor = UIColor.colorWithHexString(hex: "333333", alpha: 1)
         })
         
-        self.countryLabel = UILabel().addhere(toSuperView: self.contentView).layout(snapKitMaker: { (make) in
-            
-        }).config({ (countryL) in
-            
+        self.tF = UITextField().addhere(toSuperView: self.contentView).layout(snapKitMaker: { (make) in
+            make.top.equalToSuperview().offset(Margin)
+            make.right.equalToSuperview().offset(-Margin)
+            make.left.equalTo(self.titleLabel!.snp.right).offset(Margin)
+            make.height.equalTo(40.FloatValue.IPAD_XValue)
+        }).config({ (tF) in
+            tF.placeholder = "请输入用户名 --- 测试"
+            tF.textColor   = UIColor.colorWithHexString(hex: "999999", alpha: 1)
         })
     }
 }
