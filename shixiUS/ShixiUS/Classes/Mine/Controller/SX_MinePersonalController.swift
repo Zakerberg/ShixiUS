bed9e0f5c27fc83e081aace2078a53cc93eeef85
Zakerberg <blmichael1129@icloud.com>
2018-08-09 11:16:08 +0800

    Commit

--- shixiUS/ShixiUS/Classes/Mine/Controller/SX_MinePersonalController.swift
+++ shixiUS/ShixiUS/Classes/Mine/Controller/SX_MinePersonalController.swift
@@ -10,14 +10,78 @@ import UIKit
 
 class SX_MinePersonalController: UIViewController {
 
+    lazy var table: UITableView = {
+        let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: Int(SCREEN_WIDTH), height: Int(SCREEN_HEIGHT)), style: .plain)
+        tableView.backgroundColor = UIColor.SX_BackGroundColor()
+        tableView.showsVerticalScrollIndicator = false
+        tableView.delegate   = self
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.SX_BackGroundColor()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
+
+// ===============================================================================================================================
+// MARK: -
+// ===============================================================================================================================
+extension SX_MinePersonalController {
+    
+
+    
+    
+    
+}
+
+// ===============================================================================================================================
+// MARK: -
+// ===============================================================================================================================
+extension SX_MinePersonalController: UITableViewDelegate, UITableViewDataSource {
+   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        
+        
+        
+        
+        
+        return cell
+    }
+}
+
+// ===============================================================================================================================
+// MARK: -
+// ===============================================================================================================================
+extension SX_MinePersonalController {
+    
+ 
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
---
+++ shixiUS/ShixiUS/Classes/Mine/View/SX_Personal/SX_PersonalIconCell.swift
@@ -0,0 +1,24 @@
+//
+//  SX_PersonalIconCell.swift
+//  ShixiUS
+//
+//  Created by Michael 柏 on 8/9/18.
+//  Copyright © 2018 Shixi (Beijing)  Tchnology  Limited. All rights reserved.
+//  
+
+import UIKit
+
+class SX_PersonalIconCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
