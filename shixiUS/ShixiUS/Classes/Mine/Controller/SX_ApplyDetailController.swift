2113682120eee7d3859a1ba967ff536c7f1aeb4a
Zakerberg <blmichael1129@icloud.com>
2018-08-10 15:52:44 +0800

    Commit

--- shixiUS/ShixiUS/Classes/Mine/Controller/SX_ApplyDetailController.swift
+++ shixiUS/ShixiUS/Classes/Mine/Controller/SX_ApplyDetailController.swift
@@ -8,25 +8,95 @@
 
 import UIKit
 
+let applyDetailCellID = "applyDetailCellID"
 class SX_ApplyDetailController: UIViewController {
-
+    
+    var payBtn    : UIButton?
+    var cancelBtn : UIButton?
+    
+    lazy var table: UITableView = {
+        let tableView = UITableView(frame: CGRect(x: 0, y: 0, width: Int(SCREEN_WIDTH), height: Int(SCREEN_HEIGHT)), style: .grouped)
+        tableView.backgroundColor = UIColor.SX_BackGroundColor()
+        tableView.showsVerticalScrollIndicator = false
+        tableView.delegate   = self
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 }
 
+// ===============================================================================================================================
+// MARK: - Other Method
+// ===============================================================================================================================
+extension SX_ApplyDetailController {
+  
+    func setUI() {
+        
+    }
+    
+    func fetchData() {
+        
+    }
+}
 
+// ===============================================================================================================================
+// MARK: - UITableViewDelegate
+// ===============================================================================================================================
+extension SX_ApplyDetailController: UITableViewDelegate,UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if section == 2 {
+            return 4
+        }
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .default, reuseIdentifier: applyDetailCellID)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return UIView()
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 45.FloatValue.IPAD_XValue
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.section == 0 {
+            SXLog("进入申请进度详情界面")
+        }
+    }
+}
 
-
-
-
-
-
-
+// ===============================================================================================================================
+// MARK: -
+// ===============================================================================================================================
+extension SX_ApplyDetailController {
+    
+    
+    
+    
+    
+}
--- shixiUS/ShixiUS/Classes/Mine/Controller/SX_ProgressDetailController.swift
+++ shixiUS/ShixiUS/Classes/Mine/Controller/SX_ProgressDetailController.swift
@@ -10,26 +10,37 @@ import UIKit
 
 class SX_ProgressDetailController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+
+// ===============================================================================================================================
+// MARK: - Other Method
+// ===============================================================================================================================
+extension SX_ProgressDetailController {
     
+    
+    
+    
+    
+}
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+// ===============================================================================================================================
+// MARK: - UITableViewDelegate
+// ===============================================================================================================================
+extension SX_ProgressDetailController {
+   
+
+
+
 
 }
