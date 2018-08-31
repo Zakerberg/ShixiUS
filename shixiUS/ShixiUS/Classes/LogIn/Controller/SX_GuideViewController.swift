5e91da815a5dc9538577716cdf5d1c9f8b700a95
Zakerberg <blmichael1129@icloud.com>
2018-08-31 15:58:05 +0800

    Commit

--- shixiUS/ShixiUS/Classes/LogIn/Controller/SX_GuideViewController.swift
+++ shixiUS/ShixiUS/Classes/LogIn/Controller/SX_GuideViewController.swift
@@ -15,9 +15,27 @@
 import UIKit
 
 class SX_GuideViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
+    }
+}
 
+// =========================================================================================================================
+// MARK: - Other Method
+// =========================================================================================================================
+extension SX_GuideViewController {
+    
+    func setUI() {
+        // gif和jpg类型的资源数组
+        let imageGifArray = ["guideImage1.jpg","guideImage6.gif", "guideImage8.gif", "guideImage2.jpg","guideImage7.gif", "guideImage5.jpg"]
+        let guideView = SX_GuidePageView.init(images: imageGifArray, loginRegistCompletion: {
+            SXLog("登录/注册")
+        }) {
+            SXLog("开始使用App")
+        }
+        
+        self.view.addSubview(guideView)
     }
 }
