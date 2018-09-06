fd8eab23c958966ba61731db342789bc06313065
Zakerberg <blmichael1129@icloud.com>
2018-09-06 17:03:10 +0800

    area

--- shixiUS/ShixiUS/Classes/Other/SX_Picker/SX_AdressPickerView.swift
+++ shixiUS/ShixiUS/Classes/Other/SX_Picker/SX_AdressPickerView.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class SX_AdressPickerView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 }
