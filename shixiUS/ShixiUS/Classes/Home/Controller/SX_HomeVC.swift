786b24263838e3919cb75db87c7be090da2499a7
Zakerberg <blmichael1129@icloud.com>
2018-11-16 15:28:54 +0800

    Commit

--- shixiUS/ShixiUS/Classes/Home/Controller/SX_HomeVC.swift
+++ shixiUS/ShixiUS/Classes/Home/Controller/SX_HomeVC.swift
@@ -426,6 +426,16 @@
         }
         return UIView()
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        if indexPath.section == 1 {
+        let model = jobsArr[indexPath.row]
+        let vc = SX_HotJobDetailController()
+        vc.id  = model["id"].string
+        self.navigationController?.pushViewController(vc, animated: true)
+        }
+    }
  }
  
  // ===============================================================================
